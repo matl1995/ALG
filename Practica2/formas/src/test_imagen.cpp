1c9b8ed632983e32d719a149ad064fcb8a966ca1
Unknown <matl1995@correo.ugr.es>
2018-04-10 17:57:03 +0200

    Changes in forms exercise(not working)

--- Practica2/formas/src/test_imagen.cpp
+++ Practica2/formas/src/test_imagen.cpp
@@ -1,132 +1,69 @@
 #include "imagen.h"
 #include <fstream>
 
-int CuantasFormas(Imagen &I,Imagen &forma,int inicio_i,int inicio_j,int tam,Imagen &mask)
+int CuantasFormas(Imagen &I,Imagen &forma,int inicio_i,int inicio_j,int tam_f,int tam_c,int **mask)
 {
   int resultado;
 
-  if(I.num_filas()<forma.num_filas() || I.num_cols()<forma.num_cols())
+  if(tam_f<forma.num_filas() || tam_c<forma.num_cols())
   {
     resultado=0;
   }
-  else if(I.num_filas()==forma.num_filas() && I.num_cols()==forma.num_cols())
+  else if(tam_f==forma.num_filas() && tam_c==forma.num_cols())
   {
     bool coincide=true;
+    bool ya_contada=true;
 
-    for(int i=0;i<I.num_filas();i++)
+    for(int i=inicio_i;i<tam_f;i++)
     {
-      for(int j=0;j<I.num_cols();j++)
+      for(int j=inicio_j;j<tam_c;j++)
       {
         if(I(i,j)!=forma(i,j))
           coincide=false;
+
+        if(mask[i][j]==0)
+          ya_contada=false;
       }
     }
 
     if(!coincide)
       resultado=0;
-    else
+    else if(coincide && !ya_contada)
     {
       resultado=1;
 
       //Establezco los valores de la mascara a 1
-      for(int i=0;i<mask.num_filas();i++)
+      for(int i=inicio_i;i<tam_f;i++)
       {
-        for(int j=0;j<mask.num_cols();j++)
+        for(int j=inicio_j;j<tam_c;j++)
         {
-          mask(i,j)='255';
+          mask[i][j]=255;
         }
       }
     }
   }
   else
   {
-    /*Creo las imagenes de los subcuadrantes a inspeccionar y de las zonas cerca del eje*/
+    //Hago las recurrencias de los cuatro cuadrantes
+    int n1=CuantasFormas(I,forma,inicio_i,inicio_j,tam_f/2,tam_c/2,mask);
 
-    //Cuadrantes superiores
-    Imagen i1(I.num_filas()/2+I.num_filas()%2,I.num_cols()/2+I.num_cols()%2);
-    Imagen i2(I.num_filas()/2+I.num_filas()%2,I.num_cols()/2);
+    int n2=CuantasFormas(I,forma,inicio_i,inicio_j+tam_c/2,tam_f/2,tam_c/2,mask);
 
-    //Cuadrantes inferiores
-    Imagen i3(I.num_filas()/2,I.num_cols()/2+I.num_cols()%2);
-    Imagen i4(I.num_filas()/2,I.num_cols()/2);
+    int n3=CuantasFormas(I,forma,inicio_i+tam_f/2,inicio_j,tam_f/2,tam_c/2,mask);
 
-    /*pongo en los nuevos cuadrantes el estracto deseado de la original*/
+    int n4=CuantasFormas(I,forma,inicio_i+tam_f/2,inicio_j+tam_c/2,tam_f/2,tam_c/2,mask);
 
-    i1.PutImagen(0,0,I);
-    
-    for(int i=0;i<i2.num_filas();i++)
-    {
-    	for(int j=0;j<i2.num_cols();j++)
-    	{
-    		i2(i,j)=I(i+I.num_filas()/2+I.num_filas()%2,j);
-    	}
-    }
+    //Hago las recurrencias de la franja horizontal
+    int n5=CuantasFormas(I,forma,inicio_i+tam_f/2-forma.num_filas()+1,inicio_j,(forma.num_filas()*2)-2,tam_c/2,mask);
 
-    for(int i=0;i<i3.num_filas();i++)
-    {
-    	for(int j=0;j<i3.num_cols();j++)
-    	{
-    		i3(i,j)=I(i,j+I.num_cols()/2+I.num_cols()%2);
-    	}
-    }
+    int n6=CuantasFormas(I,forma,inicio_i+tam_f/2-forma.num_filas()+1,inicio_j+tam_c/2,(forma.num_filas()*2)-2,tam_c/2,mask);
 
-    for(int i=0;i<i4.num_filas();i++)
-    {
-    	for(int j=0;j<i4.num_cols();j++)
-    	{
-    		i4(i,j)=I(i+I.num_filas()/2,j+I.num_cols()/2);
-    	}
-    }
+    //Hago las recurrencias de la franja vertical
+    int n7=CuantasFormas(I,forma,inicio_i,inicio_j+tam_c/2-forma.num_cols()+1,tam_f/2,(forma.num_cols()*2)-2,mask);
 
-    int num_cuad_1=formas(i1,forma);
-    int num_cuad_2=formas(i2,forma);
-    int num_cuad_3=formas(i3,forma);
-    int num_cuad_4=formas(i4,forma);
+    int n8=CuantasFormas(I,forma,inicio_i+tam_f/2,inicio_j+tam_c/2-forma.num_cols()+1,tam_f/2,(forma.num_cols()*2)-2,mask);
 
-    /*Ahora compruebo en la franjas alrededor de los limites entre subcuadrantes*/
-    bool coincide;
-
-    int num_franja_h=0;
-    //Fraja horizontal
-    for(int i=(I.num_filas()/2)-forma.num_filas();i<I.num_filas()/2;i++)
-    {
-    	for(int j=0;j<I.num_cols();j++)
-    	{
-    		coincide=true;
-    		for(int l=0;l<forma.num_filas();l++)
-    		{
-    			for(int m=0;m<forma.num_cols();m++)
-    			{
-    				if(I(i+l,j+m)!=forma(l,m))
-          		coincide=false;
-    			}
-    		}
-    		if(coincide)
-		      num_franja_h+=1;
-    	}
-    }
-
-    int num_franja_v=0;
-    //Fraja vertical
-    for(int i=0;i<I.num_filas();i++)
-    {
-    	for(int j=(I.num_cols()/2)-forma.num_cols();j<I.num_cols()/2;j++)
-    	{
-    		coincide=true;
-    		for(int l=0;l<forma.num_filas();l++)
-    		{
-    			for(int m=0;m<forma.num_cols();m++)
-    			{
-    				if(I(i+l,j+m)!=forma(l,m))
-          		coincide=false;
-    			}
-    		}
-    		if(coincide)
-		      num_franja_v+=1;
-    	}
-    }
-
-    resultado=num_cuad_1+num_cuad_2+num_cuad_3+num_cuad_4+num_franja_h+num_franja_h;
+    resultado=n1+n2+n3+n4+n5+n6+n7+n8;
   }
 
   return resultado;
@@ -144,20 +81,24 @@ int main(int argc, char * argv[]){
   I.LeerImagen(argv[1]);
   forma.LeerImagen(argv[2]);
 
-  //Genero la imagen máscara
-  Imagen mask(I.num_filas(),I.num_cols());
-
-  //Inicializo a 0 la matriz mascara
-  for(int i=0;i<mask.num_filas();i++)
+  //Reservo la memoria para la mascara
+  int **mask=new int*[I.num_filas()];
+  for(int i=0;i<I.num_filas();i++)
   {
-    for(int j=0;j<mask.num_cols();j++)
+      mask[i]=new int[I.num_cols()];
+  }
+  
+  //Relleno la mascara
+  for (int i=0;i<I.num_filas();i++)  // Recorrer vector
+  {
+    for(int j=0;j<I.num_cols();j++)
     {
-      mask(i,j)='0';
+      mask[i][j]=0;    // Generar aleatorio [0,tam]
     }
   }
   
   //Llamo a la función que contabiliza el número de formas  
-  int num_formas=CuantasFormas(I,forma,0,0,I.num_filas(),mask);
+  int num_formas=CuantasFormas(I,forma,0,0,I.num_filas(),I.num_cols(),mask);
 
   //Imprimo por pantalla el numero de veces que aparece la forma buscada
   cout<<"El numero de ocurrencias de la forma buscada es: "<<num_formas<<endl;
