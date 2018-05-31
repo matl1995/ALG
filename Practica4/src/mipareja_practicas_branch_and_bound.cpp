bd71f949c4f30358f22740eae9558afa39a8869d
Miguel Angel Torres <matl25795@gmail.com>
2018-05-31 18:49:54 +0200

    Edited Makefile, backtracking and B&B
    Edited Makefile to add backtracking and B&B programs.
    
    Finished first part backtracking exercise.
    
    Created skeleton for B&B exercise.

---
+++ Practica4/src/mipareja_practicas_branch_and_bound.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include "Apermutacion.h"
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+istream &operator>>(istream& is, vector<vector<int>> &M) 
+{
+	string linea;	//String que contiene una linea del archivo
+	istringstream entrada_str;	//Contendrá la linea pasada a string
+
+	getline(is, linea);	//Desecho la primera linea del archivo
+	getline(is, linea);	//Cojo la segunda en la que tengo el tamaño de la matriz
+
+	entrada_str.str(linea);
+
+	int fil,col;
+	entrada_str>>fil; //Introduzco el numero de filas desde el archivo
+	entrada_str>>col; //Introduzco el numero de columnas desde el archivo
+
+	//Creo la matriz
+	for(int i=0;i<fil;i++)
+	{
+    	vector<int> tmp;
+
+    	getline(is, linea);	//Obtengo la linea siguiente
+		entrada_str.str(linea); //Contiene la linea en string
+		entrada_str.clear();	//Limpio los flags de error   
+
+    	for(int j=0;j<col;j++)
+    	{
+        	int tmp1;
+        	entrada_str>>tmp1; //Meto cada elemento de la linea en la variable tmp1
+        	tmp.push_back(tmp1); //Voy creando un vector con los elementos de la linea
+    	}
+    	M.push_back(tmp); //Inserto el vector como una fila de la matriz
+	}
+
+	return is;
+}
+
+vector<vector<int>> calcularMatrizDiscrepancias(vector<vector<int>> M)
+{
+	int size=M.size();
+
+	//Creo la matriz con su tamaño reservado
+	vector<vector<int>> resultado;
+	resultado.resize(size);
+	for(unsigned int i=0;i<resultado.size();i++)
+	{
+		resultado[i].resize(size);
+	}
+
+	//Relleno la diagonal con infinito
+	for(int i=0;i<size;i++)
+	{
+		resultado[i][i]=numeric_limits<int>::max();
+	}
+
+	for(unsigned int i=0;i<M.size()-1;i++) //Bucle para recorrer cada alumno
+	{
+		for(unsigned int j=i+1;j<M.size();j++) //Bucle para comparar cada alumno con el resto
+		{
+			int discrepancia=0;
+			for(unsigned int k=0;k<M[0].size();k++) //Bucle para calcular cada una de las discrepancias
+			{
+				discrepancia+=abs(M[i][k]-M[j][k]);
+			}
+			resultado[i][j]=resultado[j][i]=discrepancia; //Introduzco la discrepancia en i,j y j,i ya que es el mismo par
+		}
+	}
+
+	return resultado;
+}
+
+int main (int argc, char * argv[]) {
+
+	if(argc!=2)
+  	{
+		cout<<"Formato para ejecución: ./mipareja_practicas_voraz gustos.txt"<< argv[1] << endl;
+	  	exit(-1);
+	}
+
+	ifstream archivo(argv[1]);
+
+	if (!archivo){
+		cout<<"No se pudo abrir el fichero "<<argv[1]<<endl;
+	}
+	  
+	vector<vector<int>> M;
+
+	archivo>>M; //Lleno la matriz con los datos del archivo
+
+	//Imprimo la matriz
+	cout<<"Archivo leido: "<<endl;
+	int fil=M.size();
+	int col=M[0].size();
+
+	cout<<fil<<" "<<col<<endl;
+	for(int i=0;i<fil;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			cout<<M[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+	//Obtengo la matriz de discrepancias entre los alumnos
+	vector<vector<int>> discrepancias=calcularMatrizDiscrepancias(M);
+
+	//Imprimo la matriz de discrepancias
+	cout<<"Matriz de discrepancias: "<<endl;
+	for(unsigned int i=0;i<discrepancias.size();i++)
+	{
+		for(unsigned int j=0;j<discrepancias[0].size();j++)
+		{
+			cout<<discrepancias[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+}
